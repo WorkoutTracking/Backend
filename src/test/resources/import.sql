da2cbc315c4debbe6739009956f5c79f7a4be911
Carlo van Kessel <carlovankessel@yahoo.nl>
2022-11-02 11:48:38 +0100

    Added resources to tests

---
+++ src/test/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO useraccount (id, name, email, role) VALUES ('7eb46918-f9ea-484d-a42d-08b68c4b0c1f', 'kees', 'kees@gamail.com', 'user');
+INSERT INTO useraccount (id, name, email, role) VALUES ('473cc7ef-f4a8-4325-bda5-a4847938313c', 'Carlo', 'carlovankessel@yahoo.nl', 'admin');
